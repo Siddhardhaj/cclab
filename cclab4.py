56e0399983541a5639e95eefcd737c90b4b14c6c
Siddhardh <berasiddu@gmail.com>
2025-12-27 11:55:20 +0530

    Update and rename cclab-1 to cclab4.py

---
+++ cclab4.py
@@ -0,0 +1,11 @@
+def travel_budget(travel_guide):
+  total_cost = 0
+  for state,cost in travel_guide.items():
+    total_cost += state[cost]
+    print(f"{state}:{cost}")
+
+travel_guide = ["Chennai":12000, "Mumbai":13000, "Delhi":14000]
+
+def(travel_guide)
+
+print(f"Total Cost : {total_cost}")
