53ecfa3a1e65ea55a9eba731fe4268a6ebcf3756
Siddhardh <berasiddu@gmail.com>
2025-12-27 11:57:28 +0530

    Fix travel_guide definition and function call

--- cclab4.py
+++ cclab4.py
@@ -4,8 +4,8 @@ def travel_budget(travel_guide):
     total_cost += state[cost]
     print(f"{state}:{cost}")
 
-travel_guide = ["Chennai":12000, "Mumbai":13000, "Delhi":14000]
+travel_guide = {"Chennai":12000, "Mumbai":13000, "Delhi":14000}
 
-def(travel_guide)
+travel_budget(travel_guide)
 
 print(f"Total Cost : {total_cost}")
