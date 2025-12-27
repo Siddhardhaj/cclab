59fe982b2c40ca9d904c8968ce38a682538a2639
Siddhardh <berasiddu@gmail.com>
2025-12-27 12:01:13 +0530

    Update cclab4.py

--- cclab4.py
+++ cclab4.py
@@ -1,12 +1,16 @@
+
+
 def travel_budget(travel_guide):
-  total_cost = 0
-  for state,cost in travel_guide.items():
-    total_cost += cost
-    print(f"{state}:{cost}")
-  return total_cost
+    total_cost = 0
+    for state, cost in travel_guide.items():
+        total_cost += cost
+        print(f"{state}: {cost}")
+    return total_cost 
 
-travel_guide = {"Chennai":12000, "Mumbai":13000, "Delhi":14000}
 
-travel_budget(travel_guide)
+travel_guide = {"Chennai": 12000, "Mumbai": 13000, "Delhi": 14000}
 
-print(f"Total Cost : {total_cost}")
+total = travel_budget(travel_guide)
+
+print(f"Total Cost: {total}")
+
